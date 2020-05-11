d99b20c4ee67ccc1384369416b2097d80c5c9081
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-05-11 15:40:44 +0200

    Add Footer Component and Social Links

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { socialMedia } from "./../../api/fake";
+import { Fade } from "react-reveal";
+
+import "./Footer.css";
+
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="footer-main">
+        <div className="social-media-div">
+          <a
+            href={socialMedia.github}
+            className="icon-button github"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-github"></i>
+            <span></span>
+          </a>
+          <a
+            href={socialMedia.linkedin}
+            className="icon-button linkedin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-linkedin"></i>
+            <span></span>
+          </a>
+          <a
+            href={`mailto:${socialMedia.gmail}`}
+            className="icon-button google"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-google"></i>
+            <span></span>
+          </a>
+          <a
+            href={socialMedia.facebook}
+            className="icon-button facebook"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-facebook-f"></i>
+            <span></span>
+          </a>
+        </div>
+        <Fade bottom duration={1000} distance="5px">
+          <div className="footer-div">
+            <p className="footer-text">
+              Made with{" "}
+              <span role="img" aria-label="heart emoji">
+                ❤️
+              </span>{" "}
+              by Abdallah Hemdan &copy; 2020
+            </p>
+          </div>
+        </Fade>
+      </div>
+    );
+  }
+}
+
+export default Footer;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,6 +3,8 @@ import ProjectCard from "./../ProjectCard/ProjectCard";
 import InstaBtn from "./../InstaBtn/InstaBtn";
 
 import { projectsCards } from "./../../api/fake";
+import { Link } from "react-router-dom";
+import { Fade } from "react-reveal";
 
 import "./Projects.css";
 
@@ -13,38 +15,47 @@ class Projects extends Component {
   }
   render() {
     return (
-      <div className="projects-main" id="projects">
-        <div className="project-main-div">
-          <div className="project-header">
-            <h1 className="heading project-heading">Main Projects</h1>
-            <p className="project-subtitle">LIST OF MY OPEN SOURCE PROJECTS</p>
+      <Fade left duration={2000} distance="100px">
+        <div className="projects-main" id="projects">
+          <div className="project-main-div">
+            <div className="project-header">
+              <h1 className="heading project-heading">Main Projects</h1>
+              <p className="project-subtitle">
+                LIST OF MY OPEN SOURCE PROJECTS
+              </p>
+            </div>
+            <div className="project-cards-div">
+              {projectsCards.map((card, index) => {
+                return (
+                  <Link
+                    to={`/project/${index}`}
+                    className="project-link"
+                    key={index}
+                  >
+                    <ProjectCard
+                      cardInfo={{
+                        title: card.title,
+                        description: card.subtitle,
+                        image: card.image,
+                        footer: card.footerLink,
+                        language: card.language,
+                        stars: card.stars,
+                        forks: card.forks,
+                      }}
+                    />
+                  </Link>
+                );
+              })}
+            </div>
+            <InstaBtn
+              text={"More Projects"}
+              className="project-button"
+              href="https://github.com/AbdallahHemdan"
+              newTab={true}
+            />
           </div>
-          <div className="project-cards-div">
-            {projectsCards.map((card, index) => {
-              return (
-                <ProjectCard
-                  key={index}
-                  cardInfo={{
-                    title: card.title,
-                    description: card.subtitle,
-                    image: card.image,
-                    footer: card.footerLink,
-                    language: card.language,
-                    stars: card.stars,
-                    forks: card.forks,
-                  }}
-                />
-              );
-            })}
-          </div>
-          <InstaBtn
-            text={"More Projects"}
-            className="project-button"
-            href="https://github.com/AbdallahHemdan"
-            newTab={true}
-          />
         </div>
-      </div>
+      </Fade>
     );
   }
 }
