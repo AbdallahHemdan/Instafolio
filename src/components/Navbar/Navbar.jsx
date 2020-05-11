8090b7e91999c62fc9897114337108918d003b53
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-05-11 05:50:03 +0200

    Add Fade Effect for the Navbar and Toggle button styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./Navbar.css";
+import Fade from "react-reveal/Fade";
 
 class Navbar extends Component {
   constructor(props) {
@@ -8,34 +9,38 @@ class Navbar extends Component {
   }
   render() {
     return (
-      <div className="nav-main fixed-top">
-        <a href="/" className="brand">
-          <span className="bracket"> &lt;</span>
-          <span className="brand-name">
-            <span className="first-name">Abdallah </span>
-            <span className="last-name">Hemdan</span>
-          </span>
-          <span className="bracket">/&gt;</span>
-        </a>
-        <input className="items-btn" type="checkbox" id="items-btn" />
-        <label className="items-icon" htmlFor="items-btn">
-          <span className="nav-icon"></span>
-        </label>
-        <ul className="items">
-          <li className="home-item">
-            <a href="/">Home</a>
-          </li>
-          <li className="about-item">
-            <a href="#about">About</a>
-          </li>
-          <li className="projects-item">
-            <a href="#projects">Projects</a>
-          </li>
-          <li className="contact-item">
-            <a href="#contact">Contact Me</a>
-          </li>
-        </ul>
-      </div>
+      <>
+        <Fade top>
+          <div className="nav-main fixed-top">
+            <a href="/" className="brand">
+              <span className="bracket">{"<"}</span>
+              <span className="brand-name">
+                <span className="first-name">Abdallah </span>
+                <span className="last-name">Hemdan</span>
+              </span>
+              <span className="bracket">{"/>"}</span>
+            </a>
+            <input className="items-btn" type="checkbox" id="items-btn" />
+            <label className="items-icon" htmlFor="items-btn">
+              <span className="nav-icon"></span>
+            </label>
+            <ul className="items">
+              <li className="home-item">
+                <a href="/">Home</a>
+              </li>
+              <li className="about-item">
+                <a href="#about">About</a>
+              </li>
+              <li className="projects-item">
+                <a href="#projects">Projects</a>
+              </li>
+              <li className="contact-item">
+                <a href="#contact">Contact Me</a>
+              </li>
+            </ul>
+          </div>
+        </Fade>
+      </>
     );
   }
 }
